e2efcfdb377c99a0a17cb20e6e95804430e7ab60
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-09-19 15:02:31 -0500

    AC-233 map view finished

--- src/src/components/searchBar/SearchBar.js
+++ src/src/components/searchBar/SearchBar.js
@@ -8,18 +8,18 @@ function SearchBar({ stations, onStationClick, bigSize, dashboard }) {
 
   const handleFilterChange = (e) => {
     setFilterText(e.target.value);
-    setSelectedStation(null); // Reiniciar la selección cuando se empieza a escribir de nuevo
+    setSelectedStation(null); // Reiniciar la selección al cambiar el filtro
   };
 
-  // Asegurarse de que las estaciones no estén duplicadas
-  const filteredStations = stations
-    .filter(
-      (station, index, self) =>
-        self.findIndex((s) => s.id === station.id) === index
-    ) // Evitar duplicados
-    .filter((station) =>
+  // Filtrar estaciones sin duplicados y basándose en el texto de filtro
+  const filteredStations = React.useMemo(() => {
+    const uniqueStations = Array.from(
+      new Map(stations.map((station) => [station.id, station])).values()
+    ); // Eliminar duplicados
+    return uniqueStations.filter((station) =>
       station.name.toLowerCase().includes(filterText.toLowerCase())
     );
+  }, [stations, filterText]);
 
   const handleStationClick = (station) => {
     if (onStationClick) {
@@ -30,48 +30,43 @@ function SearchBar({ stations, onStationClick, bigSize, dashboard }) {
   };
 
   return (
-    <>
-      <div className={`${dashboard?"":"bar-hints"}`}>
-        <div
-          className={`d-flex px-3 bg-white align-items-center border-bottom justify-content-between py-2 ${
-            filterText === "" || filterText.length < 3
-              ? "search-bar-unfocused "
-              : "search-bar-focused "
-          } ${bigSize ? "search-bar-big" : ""}`}
-        >
-          <input
-            type="search"
-            className="form-control form-control-sm border-0 text-input w-100 text-capitalize"
-            style={{ width: "350px" }}
-            aria-label="Escriba una estación"
-            placeholder={
-              selectedStation
-                ? `Seleccionado: ${selectedStation.name}`
-                : "Escriba una estación"
-            }
-            onChange={handleFilterChange}
-            value={filterText}
-            autoFocus
-          />
-          <IconSearch />
-        </div>
-
-        {filterText !== "" && filterText.length >= 3 && (
-          <div className="bg-white stations-search px-3 py-1 position-absolute w-100">
-            {filteredStations.map((station) => (
-              <div
-                className="py-1 small hint-div text-capitalize"
-                key={station.id} // Usar `station.id` como clave única
-                onClick={() => handleStationClick(station)}
-              >
-                <IconRouter className="me-3" style={{ color: "#000" }} />
-                {station.name.toLowerCase()}
-              </div>
-            ))}
-          </div>
-        )}
+    <div className={`${dashboard ? "" : "bar-hints"}`}>
+      <div
+        className={`d-flex px-3 bg-white align-items-center border-bottom justify-content-between py-2 ${
+          filterText.length < 3 ? "search-bar-unfocused" : "search-bar-focused"
+        } ${bigSize ? "search-bar-big" : ""}`}
+      >
+        <input
+          type="search"
+          className="form-control form-control-sm border-0 text-input w-100 text-capitalize text-dark"
+          aria-label="Escriba una estación"
+          placeholder={
+            selectedStation
+              ? `Seleccionado: ${selectedStation.name}`
+              : "Escriba una estación"
+          }
+          onChange={handleFilterChange}
+          value={filterText}
+          autoFocus
+        />
+        <IconSearch color="#283618" />
       </div>
-    </>
+
+      {filterText.length >= 3 && (
+        <div className="bg-white stations-search px-3 py-1 position-absolute w-100">
+          {filteredStations.map((station) => (
+            <div
+              className="py-1 small hint-div text-capitalize text-dark"
+              key={station.id}
+              onClick={() => handleStationClick(station)}
+            >
+              <IconRouter className="me-3" style={{ color: "#283618" }} />
+              {station.name.toLowerCase()}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/src/pages/station/Station.js
+++ src/src/pages/station/Station.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Station.css";
+import { Modal, Spinner } from "react-bootstrap";
 import {
   MapContainer,
   Marker,
@@ -16,8 +17,8 @@ import SearchBar from "../../components/searchBar/SearchBar";
 
 function Station() {
   const [stations, setStations] = useState([]);
-  const [dailyData, setDailyData] = useState([]);
-  const [loading, setLoading] = useState([]);
+  const [loading, setLoading] = useState(true); // Manejo de estado de carga
+  const [error, setError] = useState(null); // Manejo de errores
   const mapRef = useRef(null);
 
   const iconMarker = new L.Icon({
@@ -26,36 +27,21 @@ function Station() {
   });
 
   useEffect(() => {
-    //Call to API to get stations
+    // Llamada a la API para obtener las estaciones
     Services.getAllWeatherStations()
       .then((response) => {
-        const filteredResponseByCHIRPS = response.filter(item => item.origin === "CHIRPS y ERA-5");
-        setStations(filteredResponseByCHIRPS);
+        const filteredStations = response.filter(
+          (item) => item.origin === "CHIRPS y ERA-5"
+        );
+        setStations(filteredStations);
+        setLoading(false); // Marcar como cargado
       })
       .catch((error) => {
-        console.error(error);
+        console.error("Error al cargar las estaciones:", error);
+        setError("Error al cargar las estaciones");
+        setLoading(false); // Marcar como cargado aunque haya error
       });
   }, []);
-
-  // useEffect(() => {
-  //   if (stations.length > 0) {
-  //     const requests = stations.map((item) =>
-  //       Services.get_daily_weather(12, 2022, item.id)
-  //     );
-
-  //     Promise.all(requests)
-  //       .then((responses) => {
-  //         const data = responses.map((response) => response);
-  //         setDailyData(data);
-  //       })
-  //       .catch((error) => {
-  //         console.error(error);
-  //       })
-  //       .finally(() => {
-  //         setLoading(false);
-  //       });
-  //   }
-  // }, [stations.length]);
 
   const MapCenter = ({ stations }) => {
     const map = useMap();
@@ -65,11 +51,11 @@ function Station() {
         const bounds = L.latLngBounds(
           stations.map((station) => [station.latitude, station.longitude])
         );
-        map.fitBounds(bounds); // Adjust the map to fit the bounds
+        map.fitBounds(bounds); // Ajustar el mapa a los límites de las estaciones
       }
     }, [stations, map]);
 
-    return null; // No need to render anything
+    return null; // No es necesario renderizar nada
   };
 
   const handleStationClick = (station) => {
@@ -79,75 +65,85 @@ function Station() {
     }
   };
 
-  const popupData = (station) => {
-    return (
-      <Marker
-        position={[station.latitude, station.longitude]}
-        key={station.id}
-        icon={iconMarker}
-      >
-        <Popup closeButton={false} className="popup">
-          <div>
-            <div className="d-flex align-items-center justify-content-between ">
-              <h6 className="fw-medium mb-0 text-nowrap">
-                Estacion {station.name}
-              </h6>
-            </div>
-            <p className="mt-0 mb-2">Fecha: 32</p>
+  const renderPopupData = (station) => (
+    <Marker
+      position={[station.latitude, station.longitude]}
+      key={station.id}
+      icon={iconMarker}
+    >
+      <Popup closeButton={false} className="popup ">
+        <div className="text-dark">
+          <div className="d-flex align-items-center justify-content-between">
+            <h6 className="fw-medium mb-0 text-nowrap">
+              Estación {station.name}
+            </h6>
           </div>
-          <table className="fs-6 text-nowrap w-100">
-            <tbody>
-              <tr>
-                <td className="d-flex align-items-center me-2">
-                  Temperatura maxima:
-                </td>
-                <td>432</td>
-              </tr>
-              <tr>
-                <td className="d-flex align-items-center me-2">
-                  Temperatura minima:
-                </td>
-                <td>432</td>
-              </tr>
-              <tr>
-                <td className="d-flex align-items-center me-2">
-                  Precipitacion:
-                </td>
-                <td>432</td>
-              </tr>
-              <tr>
-                <td className="d-flex align-items-center me-2">
-                  Radiacion solar:
-                </td>
-                <td>432</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="d-flex justify-content-between flex-column align-items-end mt-3">
-            <a
-              href={`/dashboard/${station.id}`}
-              className="btn btn-primary btn-sm text-white rounded-3 fw-medium d-flex align-items-center justify-content-between px-2 py-2 mx-2"
-              role="button"
-            >
-              <IconChartDonut stroke={2} className="me-2" />
-              Data
-            </a>
-          </div>
-        </Popup>
-        <Tooltip direction="right" offset={[0, -5]} opacity={1} permanent>
-          {station.name}
-        </Tooltip>
-      </Marker>
-    );
-  };
+          <p className="mt-0 mb-2">Fecha: 32</p>
+        </div>
+        <table className="fs-6 text-nowrap w-100 text-dark">
+          <tbody>
+            <tr>
+              <td className="d-flex align-items-center me-2">
+                Temperatura máxima:
+              </td>
+              <td>432</td>
+            </tr>
+            <tr>
+              <td className="d-flex align-items-center me-2">
+                Temperatura mínima:
+              </td>
+              <td>432</td>
+            </tr>
+            <tr>
+              <td className="d-flex align-items-center me-2">Precipitación:</td>
+              <td>432</td>
+            </tr>
+            <tr>
+              <td className="d-flex align-items-center me-2">
+                Radiación solar:
+              </td>
+              <td>432</td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="d-flex justify-content-between flex-column align-items-end mt-3">
+          <a
+            href={`/dashboard/${station.id}`}
+            className="btn btn-primary btn-sm text-light rounded-3 fw-medium d-flex align-items-center justify-content-between px-2 py-2 mx-2"
+            role="button"
+          >
+            <IconChartDonut stroke={2} className="me-2" />
+            Data
+          </a>
+        </div>
+      </Popup>
+      <Tooltip direction="right" offset={[0, -5]} opacity={1} permanent>
+        {station.name}
+      </Tooltip>
+    </Marker>
+  );
+
+  if (error) {
+    return <p>{error}</p>; // Mostrar mensaje de error
+  }
 
   return (
     <>
+      {/* Modal de loading */}
+      <Modal
+        show={loading}
+        backdrop="static"
+        keyboard={false}
+        centered
+        size="sm"
+      >
+        <Modal.Body className="d-flex align-items-center ">
+          <Spinner animation="border" role="status" className="me-2" />
+          Obteniendo estaciones...
+        </Modal.Body>
+      </Modal>
       <MapContainer
-        style={{
-          height: "93vh",
-          width: "100%",
-        }}
+        style={{ height: "93vh", width: "100%" }}
         className="map-monitoring"
         zoomControl={false}
         ref={mapRef}
@@ -156,13 +152,8 @@ function Station() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         <ZoomControl position="topright" />
-
-        {stations.map((station, i) => (
-          <div key={i}>{popupData(station)}</div>
-        ))}
-
+        {stations.map((station) => renderPopupData(station))}
         <MapCenter stations={stations} />
       </MapContainer>
       <SearchBar
